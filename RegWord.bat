d7ae03800e6a0a19d3008dd26393f9fefc82c512
janieandjack <87084939+janieandjack@users.noreply.github.com>
2022-06-07 09:41:36 -0700

    Update RegWord.bat

--- RegWord.bat
+++ RegWord.bat
@@ -1,26 +1,26 @@
-#Check to see if temp folder exists and if not create it
+:: #Check to see if temp folder exists and if not create it
 
-#$path = "c:\temp\"
-#If (!(test-path $path))
-#{
-#    md $path
-#}
+:: #$path = "c:\temp\"
+:: #If (!(test-path $path))
+:: #{
+:: #    md $path
+:: #}
 
-#Write-Output "Saving current register as RegOld"
+:: #Write-Output "Saving current register as RegOld"
 
-#Back up registry key and name it RegOld.reg
-#Name back up RegOld
+:: #Back up registry key and name it RegOld.reg
+:: #Name back up RegOld
 reg export HKEY_CLASSES_ROOT\ms-msdt RegOld.reg
 
-#Move RegOld to c:\temp
-#Get-Item -Path c:\Users\Reg Test\*.reg | Move-Item -Destination c:\temp
+:: #Move RegOld to c:\temp
+:: #Get-Item -Path c:\Users\Reg Test\*.reg | Move-Item -Destination c:\temp
 
-#Write-Output "RegOld is in c:\temp"
+:: #Write-Output "RegOld is in c:\temp"
 
-#Delete
+:: #Delete
 reg delete HKEY_CLASSES_ROOT\ms-msdt /f
 
-#Write-Output "Old Reg has been deleted"
+:: #Write-Output "Old Reg has been deleted"
 
-#To Undo and restore registry key
-#reg import RegOld
+:: #To Undo and restore registry key
+:: #reg import RegOld
