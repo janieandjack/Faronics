b9aa5573ca84350271ea4afda49e8c0b06a41768
janieandjack <87084939+janieandjack@users.noreply.github.com>
2022-06-10 17:21:09 -0700

    Update Edge-Momentis.bat

--- Edge-Momentis.bat
+++ Edge-Momentis.bat
@@ -7,7 +7,7 @@ If (!(test-path $path))
 }
 
 ##Download XML file
-Invoke-WebRequest https://janieandjack.sharepoint.com/:u:/s/Janie_and_Jack/EVnM9aZgdKhDlqgYDx3aVSIBYWGX9eMvLhatX4L4WIf2zg?e=kwFil5 -Outfile c:\edge\ie_site_list.xml
+Invoke-WebRequest https://raw.githubusercontent.com/janieandjack/Faronics/main/ie_site_list.xml -Outfile c:\edge\ie_site_list.xml
 
 ##Reg commands for Edge and XML
 reg add "HKCU\SOFTWARE\Policies\Microsoft\Edge" /v InternetExplorerIntegrationLevel /t REG_DWORD /d "1" /f
